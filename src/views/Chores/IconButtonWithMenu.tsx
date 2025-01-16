7a15a0725b59f4c7effeef324e970b2a316278eb
Dany Khalife <dev@dkhalife.com>
2025-01-15 21:26:52 -0800

    iconbutton oop

--- src/views/Chores/IconButtonWithMenu.tsx
+++ src/views/Chores/IconButtonWithMenu.tsx
@@ -3,69 +3,75 @@ import IconButton from '@mui/joy/IconButton'
 import React, { useRef } from 'react'
 import { getTextColorFromBackgroundColor } from '../../utils/Colors'
 
-export const IconButtonWithMenu = ({
-  key,
-  icon,
-  options,
-  onItemSelect,
-  setSelectedItem,
-  isActive,
-  useChips,
-  title,
-}) => {
-  const menuRef = useRef(null)
+interface IconButtonWithMenuProps
+{
+  key: string,
+  icon: string,
+  options: any[],
+  onItemSelect: (item: any) => void,
+  setSelectedItem: any,
+  isActive: boolean,
+  useChips: boolean,
+  title: string,
+}
 
-  return (
-    <>
-      <IconButton
-        variant='outlined'
-        color={isActive ? 'primary' : 'neutral'}
-        size='sm'
-        sx={{
-          height: 24,
-          borderRadius: 24,
-        }}
-      >
-        {icon}
-      </IconButton>
+export class IconButtonWithMenu extends React.Component<IconButtonWithMenuProps> {
+  render(): React.ReactNode {
+    const menuRef = useRef(null)
+    const { key, icon, options, onItemSelect, setSelectedItem, isActive, useChips, title } = this.props
 
-      <Menu
-        key={key}
-        ref={menuRef}
-      >
-        {title && (
-          <MenuItem key={`${key}-title`} disabled>
-            <Typography level='body-sm' sx={{ fontWeight: 'bold' }}>
-              {title}
-            </Typography>
-          </MenuItem>
-        )}
-        {options?.map(item => (
-          <MenuItem
-            key={`${key}-${item?.id}`}
-            onClick={() => {
-              onItemSelect(item)
-              setSelectedItem?.selectedItem(item.name)
-            }}
-          >
-            {useChips ? (
-              <Chip
-                sx={{
-                  backgroundColor: item.color ? item.color : null,
-                  color: getTextColorFromBackgroundColor(item.color),
-                }}
-              >
-                {item.name}
-              </Chip>
-            ) : (
-              <>
-                {item?.icon}
-                {item.name}
-              </>
-            )}
-          </MenuItem>
-        ))}
-      </Menu>
-    </>
-  )
+    return (
+      <>
+        <IconButton
+          variant='outlined'
+          color={isActive ? 'primary' : 'neutral'}
+          size='sm'
+          sx={{
+            height: 24,
+            borderRadius: 24,
+          }}
+        >
+          {icon}
+        </IconButton>
+
+        <Menu
+          key={key}
+          ref={menuRef}
+        >
+          {title && (
+            <MenuItem key={`${key}-title`} disabled>
+              <Typography level='body-sm' sx={{ fontWeight: 'bold' }}>
+                {title}
+              </Typography>
+            </MenuItem>
+          )}
+          {options?.map(item => (
+            <MenuItem
+              key={`${key}-${item?.id}`}
+              onClick={() => {
+                onItemSelect(item)
+                setSelectedItem?.selectedItem(item.name)
+              }}
+            >
+              {useChips ? (
+                <Chip
+                  sx={{
+                    backgroundColor: item.color ? item.color : null,
+                    color: getTextColorFromBackgroundColor(item.color),
+                  }}
+                >
+                  {item.name}
+                </Chip>
+              ) : (
+                <>
+                  {item?.icon}
+                  {item.name}
+                </>
+              )}
+            </MenuItem>
+          ))}
+        </Menu>
+      </>
+    )
+  }
 }
