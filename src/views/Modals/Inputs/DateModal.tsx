3bcb957242b9e2048ae2fc34b64d8b175c3807f2
Dany Khalife <dev@dkhalife.com>
2025-01-25 19:41:09 -0800

    date modal

--- src/views/Modals/Inputs/DateModal.tsx
+++ src/views/Modals/Inputs/DateModal.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Modal, Button, Input, ModalDialog, Box, Typography } from '@mui/joy'
 import React from 'react'
 
@@ -10,17 +9,31 @@ interface DateModalProps {
   title: string
 }
 
-export class DateModal extends React.Component<DateModalProps> {
-  public render(): React.ReactNode {
-    const { isOpen, onClose, onSave, current, title } = this.props
-    const [date, setDate] = useState<string>(
-      current ? new Date(current).toISOString().split('T')[0] : "",
-    )
+interface DateModalState {
+  date: string
+}
 
-    const handleSave = () => {
-      onSave(date)
-      onClose()
+export class DateModal extends React.Component<DateModalProps, DateModalState> {
+  constructor(props: DateModalProps) {
+    super(props)
+
+    this.state = {
+      date: props.current ? new Date(props.current).toISOString().split('T')[0] : ""
     }
+  }
+
+  private handleSave = () => {
+    const { onSave, onClose } = this.props
+    const { date } = this.state
+
+    onSave(date)
+    onClose()
+  }
+
+
+  public render(): React.ReactNode {
+    const { isOpen, onClose, title } = this.props
+    const { date } = this.state
 
     return (
       <Modal open={isOpen} onClose={onClose}>
@@ -30,10 +43,10 @@ export class DateModal extends React.Component<DateModalProps> {
             sx={{ mt: 3 }}
             type='date'
             value={date}
-            onChange={e => setDate(e.target.value)}
+            onChange={e => this.setState({ date: e.target.value })}
           />
           <Box display={'flex'} justifyContent={'space-around'} mt={1}>
-            <Button onClick={handleSave} fullWidth sx={{ mr: 1 }}>
+            <Button onClick={this.handleSave} fullWidth sx={{ mr: 1 }}>
               Save
             </Button>
             <Button onClick={onClose} variant='outlined'>
