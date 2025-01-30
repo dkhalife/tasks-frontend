59a84d7dfd4a4d150588795ff5ccaadca3955dab
Dany Khalife <dev@dkhalife.com>
2025-01-30 04:21:03 -0800

    configure prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{ts,tsx}"]},
-  {ignores: ["node_modules", "dist"]},
-  {languageOptions: { globals: globals.browser }},
-  {settings: { react: { version: "detect" }}},
+  { files: ['**/*.{ts,tsx}'] },
+  { ignores: ['node_modules', 'dist'] },
+  { languageOptions: { globals: globals.browser } },
+  { settings: { react: { version: 'detect' } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
-},
-];
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
 
   resolve: {
     alias: [
