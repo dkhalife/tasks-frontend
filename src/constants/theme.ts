3760d09e105054efa09769575a48fb7b20e19461
Dany Khalife <dev@dkhalife.com>
2025-02-08 09:11:56 -0500

    remove tailwind dependency

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,10 +1,3 @@
-import resolveConfig from 'tailwindcss/resolveConfig'
-import tailwindConfig from '/tailwind.config'
-
-const { theme: THEME } = resolveConfig(tailwindConfig)
-
-export const COLORS = THEME.colors
-
 export type ThemeMode = 'light' | 'dark' | 'system'
 
 export const getNextThemeMode = (currentThemeMode: ThemeMode): ThemeMode => {
