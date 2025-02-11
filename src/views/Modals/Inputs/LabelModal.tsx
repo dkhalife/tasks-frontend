1ddab8cc3a738cf123038e60745a8c9e681bc475
Dany Khalife <dev@dkhalife.com>
2025-02-10 23:34:53 -0500

    color circle should update when new color is selected

--- src/views/Modals/Inputs/LabelModal.tsx
+++ src/views/Modals/Inputs/LabelModal.tsx
@@ -176,7 +176,6 @@ export class LabelModal extends React.Component<
               Color
             </Typography>
             <Select
-              value={color}
               onChange={this.onColorChange}
               required={true}
               defaultValue={color}
@@ -192,10 +191,10 @@ export class LabelModal extends React.Component<
                 />
               }
             >
-              {LABEL_COLORS.map(val => (
+              {LABEL_COLORS.map((opt: ColorOption) => (
                 <Option
-                  key={val.value}
-                  value={val.value}
+                  key={opt.value}
+                  value={opt}
                 >
                   <Box
                     style={{
@@ -208,9 +207,9 @@ export class LabelModal extends React.Component<
                       width={20}
                       height={20}
                       borderRadius={10}
-                      sx={{ background: val.value }}
+                      sx={{ background: opt.value }}
                     />
-                    <Typography sx={{ ml: 1 }}>{val.name}</Typography>
+                    <Typography sx={{ ml: 1 }}>{opt.name}</Typography>
                   </Box>
                 </Option>
               ))}
