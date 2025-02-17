4481509ade95a446fe847c2d3c68fe43c38fde1e
Dany Khalife <dev@dkhalife.com>
2025-02-17 14:04:04 -0800

    Remove js-cookie dependency

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,15 +1,14 @@
 import { Login } from "@/api/auth";
 import { NavigationPaths } from "./navigation";
-import Cookies from "js-cookie";
 
 export const doLogin = async (email: string, password: string, navigate: (path: string) => void) => {
   const data = await Login(email, password)
   localStorage.setItem('ca_token', data.token)
   localStorage.setItem('ca_expiration', data.expiration)
 
-  const redirectUrl = Cookies.get('ca_redirect')
+  const redirectUrl = localStorage.getItem('ca_redirect')
   if (redirectUrl) {
-    Cookies.remove('ca_redirect')
+    localStorage.removeItem('ca_redirect')
     navigate(redirectUrl)
   } else {
     navigate(NavigationPaths.MyTasks)
