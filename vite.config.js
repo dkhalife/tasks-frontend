46a25773ce339fcb7d794d4975565ac4c12b745c
Dany Khalife <dev@dkhalife.com>
2025-02-19 17:28:12 -0800

    start version at 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react-swc'
+import version from 'vite-plugin-package-version';
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), version()],
 
   resolve: {
     alias: [
