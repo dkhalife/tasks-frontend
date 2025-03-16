ca2c1a2b671176f31d8b6868011733dcb5d28cb7
Dany Khalife <dev@dkhalife.com>
2025-03-15 17:54:24 -0700

    remove console.error to let exception propagate (#122)

--- src/views/History/TaskHistory.tsx
+++ src/views/History/TaskHistory.tsx
@@ -51,8 +51,6 @@ export class TaskHistory extends React.Component<
       const data = await GetTaskHistory(this.props.taskId)
       this.setState({ taskHistory: data.history })
       this.updateHistoryInfo(data.history)
-    } catch (error) {
-      console.error('Error fetching data:', error)
     } finally {
       this.setState({ isLoading: false })
     }
