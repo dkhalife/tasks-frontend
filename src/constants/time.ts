6d0525c129b996c0ba5ce1f6b4b0de1a22a4e195
Dany Khalife <dev@dkhalife.com>
2025-08-07 16:45:12 -0700

    Consolidated constants for magic numbers (#192)

---
+++ src/constants/time.ts
@@ -0,0 +1,8 @@
+export const SECOND_MS = 1000
+export const MINUTE_MS = 60 * SECOND_MS
+export const HOUR_MS = 60 * MINUTE_MS
+export const DAY_MS = 24 * HOUR_MS
+
+export const FIVE_MINUTES_MS = 5 * MINUTE_MS
+
+export const TOKEN_REFRESH_THRESHOLD_MS = DAY_MS
